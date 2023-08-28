bec84ea11f400482f82c0d1ab40756c94e3f97ef
Overloque <poludnicyn.99@mail.ru>
2023-08-29 01:58:18 +0500

    fix video

--- src/test/java/guru/qa/helpers/Attach.java
+++ src/test/java/guru/qa/helpers/Attach.java
@@ -38,11 +38,12 @@ public class Attach {
     public static String addVideo() {
         return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
                 + getVideoUrl()
-                + "' type='video/mp4></video></body></html>";
+                + "' type='video/mp4'></video></body></html>";
     }
 
     public static URL getVideoUrl() {
         String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
+//        System.out.println(sessionId());
         try {
             return new URL(videoUrl);
         } catch (MalformedURLException e) {
