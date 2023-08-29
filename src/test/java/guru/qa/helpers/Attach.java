775dd12d135625a4d98dfcc063012497e1539314
Overloque <poludnicyn.99@mail.ru>
2023-08-30 03:08:50 +0500

    fix3

--- src/test/java/guru/qa/helpers/Attach.java
+++ src/test/java/guru/qa/helpers/Attach.java
@@ -42,7 +42,8 @@ public class Attach {
     }
 
     public static URL getVideoUrl() {
-        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
+        String selenoidUrl = System.getProperty("selenoidUrl", "selenoid.autotests.cloud");
+        String videoUrl = "https://" + selenoidUrl + "/video/" + sessionId() + ".mp4";
         try {
             return new URL(videoUrl);
         } catch (MalformedURLException e) {
